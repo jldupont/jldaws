9a7f9f4f032be8bb6a145c7eeb4cf1eb151a61b1
jldupont <github@jldupont.com>
2012-01-19 08:32:50 -0500

    started jldexec

---
+++ src/jldaws/do_checks.py
@@ -0,0 +1,17 @@
+"""
+    Created on 2012-01-19
+    @author: jldupont
+"""
+import os
+
+try:
+    import boto
+except:
+    raise Exception("* package 'boto' is required\n")
+
+
+try:
+    os.environ["AWS_ACCESS_KEY_ID"]
+    os.environ["AWS_SECRET_ACCESS_KEY"]
+except Exception, e:
+    raise Exception("* environment variable missing: %s" % str(e))
---
+++ src/jldaws/script_exec.py
@@ -0,0 +1,10 @@
+"""
+    Created on 2012-01-19
+    @author: jldupont
+"""
+import boto
+
+def run(args):
+    """
+    
+    """
