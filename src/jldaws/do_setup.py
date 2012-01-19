c75f2b0b96e5ee3ea045fb1ad5954a86864e4de9
jldupont <github@jldupont.com>
2012-01-19 17:02:41 -0500

    0.1.1

--- src/jldaws/do_setup.py
+++ src/jldaws/do_setup.py
@@ -1,5 +1,16 @@
 """
     @author: Jean-Lou Dupont
 """
+
 import logging
-logging.basicConfig(level=logging.INFO)
+
+FORMAT='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(FORMAT)
+
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+#logger=logging.getLogger()
+#console = logging.StreamHandler(sys.stdout)
+#console.setLevel(logging.INFO)
+#console.setFormatter(formatter)
+#logger.addHandler(console)
