980a985d42b47fdd8a117463ecdb0ac21f3a8c7d
jldupont <github@jldupont.com>
2012-01-19 06:45:37 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+    Jean-Lou Dupont's AWS scripts
+    
+    Created on 2012-01-19
+    @author: jldupont
+"""
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.1.0"
+
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name=         'jldaws',
+      version=      __version__,
+      description=  'Collection of Amazon AWS related scripts',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'http://www.systemical.com/doc/opensource/jldaws',
+      package_dir=  {'': "src",},
+      packages=     find_packages("src"),
+      scripts=      ['src/scripts/m3u2symlinks', 
+                     ],
+      package_data = {
+                      '':[ "*.gif", "*.png", "*.jpg" ],
+                      },
+      include_package_data=True,                      
+      zip_safe=False
+      )
